dec55224272fd46f395fcad9aa9c105a0ac97a41
giuliomarinelli <giulio.marinelli@icloud.com>
2024-07-01 19:09:28 +0200

    second commit

---
+++ my_chat_backend/my_chat/src/main/java/backend/app/my_chat/config/Config.java
@@ -0,0 +1,16 @@
+package backend.app.my_chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+}
